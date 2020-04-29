ed198da1c9f3c0da24df94ad8bcadca180b95886
aldimasnh <58907747+aldimasnh@users.noreply.github.com>
2020-04-29 21:11:42 +0700

    menambahkan menu cara pesan dan jadwal berangkat

--- index.php
+++ index.php
@@ -33,6 +33,7 @@
                 </div>
             </div>
         </nav>
+        
         <!-- Masthead-->
         <header class="masthead">
             <div class="container">
@@ -42,6 +43,56 @@
             </div>
         </header>
 
+        <!-- Cara-Pesan-->
+        <section class="page-section" id="cara-pesan">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase"><br><br>Cara Pemesanan</h2>
+                    <h3 class="section-subheading text-muted">1. Tentukan tanggal keberangkatan dan jenis armada yang akan anda pesan.<br><br>2. Kemudian, isi form untuk pengisian data customer atau pemesan.<br><br>3. Setelah data pemesan selesai diisikan, klik tombol Pesan Tiket, maka akan tampil data pemesan dan juga ada total pembayaran.<br><br>4. Lakukan konfirmasi pembayaran dan upload bukti pembayaran anda pada form yang tersedia.<br><br>5. Apabila telah melakukan pembayaran, maka tiket sudah dapat dicetak oleh pemesan.</h3>
+                </div>
+            </div><br>
+        </section>
+
+        <!-- Jadwal-Berangkat Grid-->
+        <section class="page-section bg-light" id="jadwal-berangkat">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Jadwal Keberangkatan</h2>
+                </div>
+                <table class="table text-center">
+                    <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Nama Armada</th>
+                        <th scope="col">Relasi</th>
+                        <th scope="col">Jam Keberangkatan</th>
+                        <th scope="col">Jam Kedatangan</th>
+                        <th scope="col">Tarif</th>
+                    </tr>
+                    </thead>
+                    <?php 
+                    include 'koneksi.php';
+                    $no = 1;
+                    $data = mysqli_query($koneksi,"select * from jadwal_berangkat");
+                    while($d = mysqli_fetch_array($data)){
+                        ?>
+                        <tbody>
+                        <tr>
+                            <td><?php echo $no++; ?></td>
+                            <td><?php echo $d['nama_armada']; ?></td>
+                            <td><?php echo $d['relasi']; ?></td>
+                            <td><?php echo $d['jam_berangkat']; ?></td>
+                            <td><?php echo $d['jam_datang']; ?></td>
+                            <td><?php echo "Rp. "; ?><?php echo $d['tarif']; ?></td> 
+                        </tr>
+                        </tbody>
+                        <?php 
+                    }
+                    ?>
+                </table>
+            </div><br><br>
+        </section>
+        
         <!-- Footer-->
         <footer class="footer py-4">
             <div class="container">
