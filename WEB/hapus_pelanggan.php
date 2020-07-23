b64e7f5062651610a58de3918855b3190988610d
aldimasnh <58907747+aldimasnh@users.noreply.github.com>
2020-07-23 15:36:07 +0700

    Create hapus_pelanggan.php

---
+++ WEB/hapus_pelanggan.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <script src="assets/sweet-alert2/sweetalert2.all.min.js"></script>
+       <script src="https://cdn.jsdelivr.net/npm/promise-polyfill"></script>
+       <script src="assets/sweet-alert2/sweetalert2.min.js"></script>
+       <link rel="stylesheet" href="assets/sweet-alert2/sweetalert2.min.css">
+</head>
+<body>
+<?php
+include 'koneksi.php';
+
+$id = $_GET['id'];
+
+$sql = mysqli_query($connect, 'delete from customer where id="'.$id.'" ');
+
+if($sql) {
+  echo "<script>
+              Swal.fire({
+                     icon: 'success',
+                     title: 'Successful',
+                     text: 'Data berhasil dihapus.'
+              }).then(function() {
+                     window.location.href='data_pelanggan.php';
+              });
+              </script>";
+} else {
+    echo "<script>
+              Swal.fire({
+                     icon: 'error',
+                     title: 'Oops...',
+                     text: 'Maaf, data gagal dihapus.'
+              }).then(function() {
+                     window.location.href='data_pelanggan.php';
+              });
+              </script>";  
+}
+?>
+</body>
+</html>
