72ddae1ae7f9e029b74b15f004fca6a7a86d2b8c
aldimasnh <58907747+aldimasnh@users.noreply.github.com>
2020-07-23 15:15:35 +0700

    Update and rename login.php to WEB/masuk.php

---
+++ WEB/masuk.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>e-ticket CAPITALIST</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.ico" rel="icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style-in.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <!-- ======= Top Bar ======= -->
+  <div id="topbar" class="d-none d-lg-flex align-items-center fixed-top">
+    <div class="container d-flex align-items-center justify-content-between">
+      <div class="d-flex align-items-center">
+        <i class="icofont-envelope"></i>cs.capitalist@gmail.com
+      </div>
+      <div class="d-flex align-items-center">
+        <i class="icofont-phone"></i> +62 856 7895 1234
+      </div>
+    </div>
+  </div>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo mr-auto"><a href="index.php">BOOK NOW!</a></h1>
+
+    </div>
+  </header><!-- End Header -->
+
+  <main id="main">
+    <section id="masuk" class="masuk">
+      <br><br><br><br><br><div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>MASUK PENGGUNA</h2>
+        </div>
+        
+        <form method="post" action="proses_masuk.php">
+        <div class="form-group row">
+            <label for="inputUsername3" class="col-sm-2 col-form-label">Username</label>
+            <div class="col-sm-10">
+                <input type="text" name="username" class="form-control" id="username" placeholder="Masukkan username anda" required>
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="inputPassword3" class="col-sm-2 col-form-label">Kata Sandi</label>
+            <div class="col-sm-10">
+                <input type="password" name="password" class="form-control" id="password" placeholder="Masukkan kata sandi anda" required>
+            </div>
+        </div>
+        <button type="submit" class="btn btn-dark btn-lg btn-block">Masuk</button>
+        <br><div align="right">Belum memiliki akun? <a href="daftar.php">Klik disini!</a></div>
+        </form>
+      </div>
+    </section><br><br><br><br><br>
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-top">
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>e-ticket CAPITALIST</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        Designed by <a href="#">CAPITALIST TEAM</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
